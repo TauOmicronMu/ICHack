6da178f427488e3002e13dba43402abd31dcd2b1
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2016-02-20 20:36:47 +0000

    Did stuff :>

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http')
-var port = process.env.PORT || 80;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello I am shit at this shit\n');
-}).listen(port);
